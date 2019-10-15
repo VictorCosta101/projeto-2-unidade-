7a065bf7fc74cbda3b88fc06fe7c09ac6bc88ced
victors <jose.v50@outlook.com>
2019-10-15 08:45:00 -0300

    main finalizado

--- Sculptor/main.cpp
+++ Sculptor/main.cpp
@@ -3,8 +3,18 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "cutbox.h"
+#include "putvoxel.h"
+#include "cutvoxel.h"
+#include "putbox.h"
+#include "cutbox.h"
+#include "putsphere.h"
+#include "cutsphere.h"
+#include "putellipsoid.h"
+#include "cutellipsoid.h"
+
 using namespace std;
 
 int main()
@@ -30,86 +40,97 @@ int main()
             stringstream ss(texto);
             ss >> comando;
             cout << comando << endl;
-            if(comando.compare("cutbox")==0)
+
+            if(comando.compare("dim")==0)
             {
-                int pos[3];
+                int x,y,z;
+                ss >> x >> y >> z;
+
+                s = new Sculptor(x,y,z);
+
+            }
+
+            else if (comando.compare("putvoxel")==0)
+            {
+                int x0,y0,z0;
+                float r,g,b,alpha;
+                ss >> x0 >> y0 >> z0 >> r >> g >> b >>alpha;
+                v.push_back(new PutVoxel(x0,y0,z0,r,g,b,alpha));
+            }
+
+            else if(comando.compare("cutvoxe")==0)
+            {
+                int x0, y0, z0;
+
+                ss >> x0 >> y0 >>z0;
+
+                v.push_back(new CutVoxel(x0,y0,z0));
+            }
+            else if (comando.compare("putbox")==0) {
+                int pos[6];
                 float color[4];
 
-                ss >> pos[0]>>pos[1]>>pos[2];
-                ss>>color[0]>>color[1]>>color[3]>>color[4];
+                ss >> pos[0]>>pos[1]>>pos[2]>> pos[3]>>pos[4]>>pos[5];
+                ss >> color[0] >> color[1] >> color[2] >> color[3];
 
-                for(int i = 0; i < 3; i ++)
-                {
-                    cout << pos[i]<<endl;
-                }
+                v.push_back(new PutBox(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],color[0],color[1],color[2],color[3]));
+
+            }
+            else if(comando.compare("cutbox")==0)
+            {
+                int pos[6];
+
+                ss >> pos[0]>>pos[1]>>pos[2]>> pos[3]>>pos[4]>>pos[5];
+
+                v.push_back(new CutBox(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5]));
+            }
+            else if (comando.compare("putsphere")==0)
+            {
+             int pos[4];
+             float color[4];
+
+             ss >> pos[0] >> pos[1] >> pos[2] >> pos[3];
+             ss >> color[0] >> color[1] >> color[2] >> color[3];
+             v.push_back(new PutSphere(pos[0],pos[1],pos[2],pos[3],color[0],color[1],color[2],color[3]));
+            }
+            else if (comando.compare("cutsphere")==0)
+            {
+                int pos[4];
+
+                ss >> pos[0] >>pos[1] >> pos[2] >>pos[3];
+                v.push_back(new CutSphere(pos[0],pos[1],pos[2],pos[3]));
+            }
+            else if (comando.compare("putellipsoid")==0)
+            {
+                int pos[6];
+                float color[4];
+                ss >> pos[0] >> pos[1] >> pos[2] >> pos[3] >> pos[4] >> pos[5];
+                ss >> color[0] >> color[1] >> color[2] >> color[3];
+                v.push_back(new PutEllipsoid(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],color[0],color[1],color[2],color[3]));
+
+            }
+            else if (comando.compare("cutellipsoid")==0)
+            {
+                int pos[6];
+
+                ss >> pos[0] >> pos[1] >> pos[2] >> pos[3] >> pos[4] >> pos[5];
+                v.push_back(new CutEllipsoid(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5]));
+
             }
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* int acao=0;
-    string arquivo = "objeto1";
-
-    Sculptor v(20,20,20);
-
-    v.setColor(1,0,0,1);
-
-    cout<< "putBox --------- [1]"<<endl;
-    cout<< "cutBox --------- [2]"<<endl;
-    cout<< "putSphere ------ [3]"<<endl;
-    cout<< "cutSphere ------ [4]"<<endl;
-    cout<< "putEllipsoid --- [5]"<<endl;
-    cout<< "cutEllipsoid --- [6]"<<endl;
-    cout<< "Todos ---------- [7]"<<endl;
-    cout<<"Acao: ";
-    cin>>acao;
-    switch (acao) {
-        case 1:
-            v.putBox(2,5,2,5,2,5);
-            break;
-        case 2:
-            v.cutBox(2,5,2,5,2,5);
-            break;
-        case 3:
-            v.putSphere(9,9,9,6);
-            break;
-        case 4:
-            v.cutSphere(9,9,9,6);
-            break;
-        case 5:
-            v.putEllipsoid(9,9,9,10,6,3);
-           break;
-
-
-        case 6:
-            v.cutEllipsoid(9,9,9,10,6,3);
-            break;
-        case 7:
-            v.putBox(2,5,2,5,2,5);
-            v.cutBox(2,5,2,5,2,5);
-            v.putSphere(9,9,9,6);
-            v.cutSphere(9,9,9,6);
-            v.putEllipsoid(9,9,9,10,6,3);
-            v.cutEllipsoid(9,9,9,10,6,3);
+for(int i = 0; i < v.size(); i++)
+{
+    v[i]->draw(*s);
     }
+s->writeOFF("objeto1");
+
+s->~Sculptor();
+
+return 0;
 
 
-    v.writeOFF(arquivo);*/
 
-    return 0;
+
 }
