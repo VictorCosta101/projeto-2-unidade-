b7f88d5643e84e27707b9031e98d3c1c38218500
victors <jose.v50@outlook.com>
2019-10-17 09:20:04 -0300

    comentarios

--- Sculptor/cutellipsoid.cpp
+++ Sculptor/cutellipsoid.cpp
@@ -1,16 +1,22 @@
 #include "cutellipsoid.h"
 
+//classe para cortes em forma de elipse
+
 CutEllipsoid::CutEllipsoid(int x, int y, int z, int rx, int ry , int rz)
 {
+    // dados do argumento do metodo CutEllipsoide para as variaveis da classe
     x_ = x; y_ = y; z_ = z; rx_ = rx; ry_ = ry; rz_ = rz;
 
 
 }
 
 CutEllipsoid::~CutEllipsoid()
-{}
+{
+    // destrutor da classe
+}
 
 void CutEllipsoid::draw(Sculptor &t)
 {
+   // metodo draw cria um objeto sculptor t par usar a função de desativar uma elipse
     t.cutEllipsoid(x_,y_,z_,rx_,ry_,rz_);
 }
